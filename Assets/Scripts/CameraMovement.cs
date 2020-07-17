349a842eb3286c60a18e1c582a62db8ee00dac0f
Blythe <blytheballesteros@gmail.com>
2020-07-16 21:24:25 -1000

    fixed cameras

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    Transform myTransform;
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 5, -5);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = this.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position = target.position + target.TransformDirection(offset);
+        myTransform.LookAt(target);
+    }
+}
