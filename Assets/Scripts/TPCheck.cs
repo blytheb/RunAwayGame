32be47237001a3c6807f55c812d953adae19d275
Blythe <blytheballesteros@gmail.com>
2020-07-17 14:58:40 -1000

    add and grab and collect tp

---
+++ Assets/Scripts/TPCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPCheck : MonoBehaviour
+{
+    public bool gotTP = false;
+
+    private Collider grab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grab = GetComponent<Collider>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "TP")
+        {
+            gotTP = true;
+            other.gameObject.SetActive(false);
+        }
+    }
+}
+  
